dc4b3c3ea08fbf96f4621fa5c57e39a8db5036d3
easdxcc123 <106867420+easdxcc123@users.noreply.github.com>
2024-01-13 19:43:48 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=' {"AccountTag":"6022aa780885e9063fb64c8ad75e8084","TunnelSecret":"JKY5rLDRcAy/+Y3xLJbDO9O+X3WHMLmDuHiRwtuDtU8=","TunnelID":"300eeb9d-b787-40d6-aa48-5dbbcbcfd76b"}'
+ARGO_AUTH='{"AccountTag":"6022aa780885e9063fb64c8ad75e8084","TunnelSecret":"JKY5rLDRcAy/+Y3xLJbDO9O+X3WHMLmDuHiRwtuDtU8=","TunnelID":"300eeb9d-b787-40d6-aa48-5dbbcbcfd76b"}'
 ARGO_DOMAIN=fly.qmoezoe.eu.org
 
 generate_config() {
